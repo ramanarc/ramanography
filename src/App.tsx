58e4d5f05baad699a3e082461352702a5f78cd1c
Anuj Raman <anujramanc@gmail.com>
2023-06-01 11:04:09 +0530

    semantic html & css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-// import { useState } from 'react'
+import pho from "./assets/pho.jpg";
+import aesthetic from "./assets/aesthetic.jpg";
+import ambience from "./assets/ambience.jpg";
+import fort from "./assets/fort.jpg";
+import gardens from "./assets/gardens.jpg";
+import pool from "./assets/pool.jpg";
+
+const images = [pho, aesthetic, ambience, fort, gardens, pool];
 
 function App() {
   return (
@@ -9,7 +16,7 @@ function App() {
           <h2>Photos captured on my iPhone 13</h2>
         </header>
         <figure>
-          <img src="" alt="" />
+          <img src={images[2]} alt="" />
         </figure>
       </section>
     </>
